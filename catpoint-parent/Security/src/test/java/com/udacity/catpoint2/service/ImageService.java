b2cb6560df8c4cb4c09d47568d10063c5a575ded
jniyonkuru <jiniyonkurujimmy@gmail.com>
2024-01-24 12:12:23 +0100

    add image interface

---
+++ catpoint-parent/Security/src/test/java/com/udacity/catpoint2/service/ImageService.java
@@ -0,0 +1,14 @@
+package com.udacity.catpoint2.service;
+
+import java.awt.image.BufferedImage;
+
+public interface ImageService
+{
+    /**
+     * Method to determine if an image contains a cat.
+     * @param image The image to analyze.
+     * @param confidenceLevel The confidence level for cat detection.
+     * @return true if a cat is detected, false otherwise.
+     */
+    boolean imageContainsCat(BufferedImage image, float confidenceLevel);
+}
